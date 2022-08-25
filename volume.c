12050638c92cf08ca2cd610e8029d3d2179fdb77
engnabil30 <engnabil30@users.noreply.github.com>
2022-08-25 10:59:05 +0000

    automated commit by check50 [check50=True]

--- volume.c
+++ volume.c
@@ -17,6 +17,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+
     // Open files and determine scaling factor
     FILE *input = fopen(argv[1], "r");
     if (input == NULL)
@@ -24,7 +25,6 @@ int main(int argc, char *argv[])
         printf("Could not open file.\n");
         return 1;
     }
-
     FILE *output = fopen(argv[2], "w");
     if (output == NULL)
     {
@@ -39,11 +39,11 @@ int main(int argc, char *argv[])
     fread(header, HEADER_SIZE, 1, input);
     fwrite(header, HEADER_SIZE, 1, output);
     // TODO: Read samples from input file and write updated data to output file
-    uint16_t buffer;
-    while (fread(&buffer, sizeof(uint16_t), 1, input))
+    int16_t buffer;
+    while (fread(&buffer, sizeof(int16_t), 1, input))
     {
         buffer *= factor;
-        fwrite(&buffer, sizeof(uint16_t), 1, output);
+        fwrite(&buffer, sizeof(int16_t), 1, output);
     }
     // Close files
     fclose(input);
