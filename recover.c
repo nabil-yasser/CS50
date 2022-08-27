8f1b8de5cdd5cb909a2f5524142b703e60a01672
engnabil30 <engnabil30@users.noreply.github.com>
2022-08-27 12:40:35 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
         return 1;
     }
     // Open memory Card
-    FILE* MemoryCard = fopen(argv[1], "r");
+    FILE *MemoryCard = fopen(argv[1], "r");
     if (MemoryCard == NULL)
     {
         printf("File Can't be opened\n");
         return 1;
     }
-    uint8_t* buffer = malloc(BUFFER_SIZE);
+    uint8_t *buffer = malloc(BUFFER_SIZE);
     uint32_t FileCounter = 0;
     if (buffer == NULL)
     {
@@ -25,25 +25,29 @@ int main(int argc, char *argv[])
     }
     // Repeat until the end of Card
     // Read 512 bytes into a buffer
-    char* NewName = NULL;
-    FILE* NewJpeg = NULL;
-    while (fread(buffer, BUFFER_SIZE, 1, MemoryCard) == BUFFER_SIZE)
+    FILE *NewJpeg = NULL;
+    char *NewName = malloc(8 * sizeof(uint8_t));
+    while (fread(buffer, BUFFER_SIZE, 1, MemoryCard))
     {
         // If start of new JPEG
         if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && (buffer[3] & 0xF0) == 0xE0)
         {
-            sprintf(NewName, "%03i", FileCounter);
             if (FileCounter > 0)
             {
                 fclose(NewJpeg);
             }
+            sprintf(NewName, "%03i.jpg", FileCounter);
             NewJpeg = fopen(NewName, "w");
             FileCounter++;
         }
-        fwrite(buffer, BUFFER_SIZE, 1, NewJpeg);
+        if (FileCounter > 0)
+        {
+            fwrite(buffer, BUFFER_SIZE, 1, NewJpeg);
+        }
     }
     // Close Any remaining files
     fclose(MemoryCard);
     free(buffer);
+    free(NewName);
     return 0;
 }
