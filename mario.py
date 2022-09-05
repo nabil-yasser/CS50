0d3bbc8f94145d06e249d7f68635a19819c8e66a
engnabil30 <engnabil30@users.noreply.github.com>
2022-09-05 12:42:59 +0000

    automated commit by check50 [check50=True]

---
+++ mario.py
@@ -0,0 +1,7 @@
+from cs50 import get_int
+
+height = 0
+while height not in range(1, 9):
+    height = get_int("Height: ")
+for i in range(height):
+    print("{0:>{1}}     {0}".format("#" * (i + 1), height))
