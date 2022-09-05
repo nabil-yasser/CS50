6bf463364435d9c19b00e1a5f3b2987ff8fcce0b
engnabil30 <engnabil30@users.noreply.github.com>
2022-09-05 14:12:53 +0000

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 0;
+
+    do
+    {
+        height = get_int("Height: ");
+    } while (height > 8 || height < 1);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int k = 1; k < (height - i); k++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < (i + 1); j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
