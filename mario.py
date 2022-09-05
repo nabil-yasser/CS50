33ea16522b286a600cdf56a139bcad536fdf1204
engnabil30 <engnabil30@users.noreply.github.com>
2022-09-05 12:43:59 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -4,4 +4,4 @@ height = 0
 while height not in range(1, 9):
     height = get_int("Height: ")
 for i in range(height):
-    print("{0:>{1}}     {0}".format("#" * (i + 1), height))
+    print("{0:>{1}}  {0}".format("#" * (i + 1), height))
